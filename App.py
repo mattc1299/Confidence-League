c1141b74b2be4ff64c1d551018a7c69d22a1b07f
mattc1299 <165582799+mattc1299@users.noreply.github.com>
2024-09-04 22:15:24 -0500

    Update App.py

--- App.py
+++ App.py
@@ -82,7 +82,7 @@ def getBlob(name,week):
 @st.cache_data
 def establishInputs(today):
     today=date.today()
-    startDate = date(2024,9,3,)
+    startDate = date(2024,8,27)
     if (today-startDate).days<0:
         week=1
     else:
